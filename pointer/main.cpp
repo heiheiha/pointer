42b5bfa92828f9cca7a479a79447c87d1e675bb5
heiheiha <1137029429@qq.com>
2015-11-11 21:32:00 +0800

    指针
    定义了一个指针变量，将100赋值给a变量然后输出俩行

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  pointer
+//  20141105065
 //
 //  Created by  黄磊 on 15/11/11.
 //  Copyright © 2015年  黄磊. All rights reserved.
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("1 a=%x   $a=%x   &p=%x   p=%x   *p=%x\n",a,&a,&p,p,*p);
+    printf("2 a=%x   $a=%x   &p=%x   p=%x   *p=%x\n",a,&a,&p,p,*p);
     return 0;
 }
